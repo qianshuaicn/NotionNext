e307e4ac8cb51465e9ab1bbcca8dbb355f18126b
qianshuaicn <82995417+qianshuaicn@users.noreply.github.com>
2025-03-06 21:27:33 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -62,7 +62,7 @@ const BLOG = {
   // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Hi，我是一个打工人,欢迎来到我的博客🎉',
+    '欢迎来到我的博客🎉，人民万岁',
 
   // uuid重定向至 slug
   UUID_REDIRECT: process.env.UUID_REDIRECT || false
