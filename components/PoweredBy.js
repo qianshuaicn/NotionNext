fc0b16ddbc156ed501728527e5d82764bc184afe
qianshuaicn <82995417+qianshuaicn@users.noreply.github.com>
2025-03-06 21:04:38 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -8,11 +8,6 @@ export default function PoweredBy(props) {
   return (
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
       <span className='mr-1'> </span>
-      <a
-        href='https://beian.miit.gov.cn'
-        className='underline justify-start'>
-        京ICP备2022024716号-1
-      </a>
       .
     </div>
   )
