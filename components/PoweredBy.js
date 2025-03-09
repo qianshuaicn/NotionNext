0b2fde0574fac888cfc0e813fa55fcabc19fdf9a
qianshuaicn <82995417+qianshuaicn@users.noreply.github.com>
2025-03-09 08:10:17 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -1,13 +1,13 @@
-import { siteConfig } from '@/lib/config'
+// import { siteConfig } from '@/lib/config'
 
-/**
- * 驱动版权
- * @returns
- */
-export default function PoweredBy(props) {
-  return (
-    <div className={`inline text-sm font-serif ${props.className || ''}`}>
-      <span className='mr-1'> </span>
-    </div>
-  )
-}
+// /**
+//  * 驱动版权
+//  * @returns
+//  */
+// export default function PoweredBy(props) {
+//   return (
+//     <div className={`inline text-sm font-serif ${props.className || ''}`}>
+//       <span className='mr-1'> </span>
+//     </div>
+//   )
+// }
