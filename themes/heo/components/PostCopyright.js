45d804ecf5e56f3217109e2d79eb386ee1293d15
qianshuaicn <82995417+qianshuaicn@users.noreply.github.com>
2025-03-12 09:41:14 +0800

    Update PostCopyright.js

--- themes/heo/components/PostCopyright.js
+++ themes/heo/components/PostCopyright.js
@@ -26,12 +26,6 @@ export default function PostCopyright() {
     <section className='dark:text-gray-300 mt-6 mx-1 '>
       <ul className='overflow-x-auto whitespace-nowrap text-sm dark:bg-gray-900 bg-gray-100 p-5 leading-8 border-l-2 border-indigo-500'>
         <li>
-          <strong className='mr-2'>{locale.COMMON.AUTHOR}:</strong>
-          <Link href={'/about'} className='hover:underline'>
-            {siteConfig('AUTHOR')}
-          </Link>
-        </li>
-        <li>
           <strong className='mr-2'>{locale.COMMON.URL}:</strong>
           <a
             className='whitespace-normal break-words hover:underline'
